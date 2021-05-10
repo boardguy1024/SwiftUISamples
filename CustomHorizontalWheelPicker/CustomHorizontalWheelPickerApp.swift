86108bbab8bd0c0ebcafbb4564dc5a2cb507c7f5
boardguy1024 <boardguy1024@gmail.com>
2021-05-10 22:55:27 +0900

    first commit

---
+++ CustomHorizontalWheelPicker/CustomHorizontalWheelPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomHorizontalWheelPickerApp.swift
+//  CustomHorizontalWheelPicker
+//
+//  Created by park kyung seok on 2021/05/10.
+//
+
+import SwiftUI
+
+@main
+struct CustomHorizontalWheelPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
